040f2ffe625d677d907cd952a439e0678ba6f113
DuanHuayi <huayi.duan@ymm56.com>
2020-02-08 13:01:53 +0800

    Refactor

--- sample/await.js
+++ sample/await.js
@@ -5,13 +5,22 @@
 function resolveAfter2Seconds(x) {
     return new Promise(resolve => {
         setTimeout(() => {
+            let curTime = new Date().toLocaleString();
+            console.log(`${curTime} - gogogo`);
+
             resolve(x);
         }, 2000);
     });
 }
 
 async function f1() {
+    let curTime = new Date().toLocaleString();
+    console.log(`${curTime} - 1： begin`);
+
     var x = await resolveAfter2Seconds(10);
-    console.log(x); // 10
+
+    curTime = new Date().toLocaleString();
+    console.log(`${curTime} - 1： done`);
 }
+
 f1();
